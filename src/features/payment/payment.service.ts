a60a34f645b2e1e9f6dfb9d00ff5f5bfd784071b
VuKhoiGVM <nguyenvukhoi150402@gmail.com>
2024-06-04 23:14:00 +0700

    fix: fix role create payment url

--- src/features/payment/payment.service.ts
+++ src/features/payment/payment.service.ts
@@ -34,7 +34,7 @@ export class PaymentService {
 
         this.logger.info('dataPayment');
         // check role user
-        if (user.role.toString() !== getEnumKeyByEnumValue(Role, Role.TENANT))
+        if (user.role.toString() !== getEnumKeyByEnumValue(Role, Role.USER))
             throw new GrpcPermissionDeniedException('PERMISSION_DENIED');
 
         // check order booking or order product
